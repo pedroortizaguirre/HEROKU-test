33db8e96b2bfa0bc1f7c1f2a6819071613fda63c
pedroortizaguirre <pedroortizaguirre@gmail.com>
2015-03-20 10:08:56 -0700

    Changes in the Login

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,6 +1,6 @@
 class EmployeeController < ApplicationController
 def employee
-	@employeeName= Collaborator.find('1') 
+	@employeeName= Collaborator.find('2') 
     @employeeNew = Collaborator.new
     @search = Collaborator.find_by_names(params[:search]) 
   end
