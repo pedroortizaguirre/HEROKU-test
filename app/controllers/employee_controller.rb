5e3a6234ce3f03c4e928302bbf44cfa37cbb4ecc
pedroortizaguirre <pedroortizaguirre@gmail.com>
2015-03-24 12:15:32 -0700

    Design mods

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -3,5 +3,6 @@ def employee
 	@employeeName= Collaborator.find('2') 
     @employeeNew = Collaborator.new
     @search = Collaborator.find_by_names(params[:search]) 
+     @search2 = Collaborator.where()
   end
 end
