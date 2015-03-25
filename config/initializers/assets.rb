fb60c7e3cf8a07a529c07445259d133e81c4dad2
pedroortizaguirre <pedroortizaguirre@gmail.com>
2015-03-25 10:36:59 -0700

    Design modificatios v.0.0.0.2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.precompile += %w( CSS/bootstrap.css )
-Rails.application.config.assets.precompile += %w( CSS/AdminLTE.css )
-Rails.application.config.assets.precompile += %w( CSS/skin-Mind.css )
-Rails.application.config.assets.precompile += %w( CSS/reset.css )
-Rails.application.config.assets.precompile += %w( CSS/Animationstyle.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( AdminLTE.css )
+Rails.application.config.assets.precompile += %w( skin-Mind.css )
+Rails.application.config.assets.precompile += %w( reset.css )
+Rails.application.config.assets.precompile += %w( Animationstyle.css )
 Rails.application.config.assets.precompile += %w( LoginStyle.css )
 Rails.application.config.assets.precompile += %w( bootstrap.js )
 Rails.application.config.assets.precompile += %w( dashboard.js )
